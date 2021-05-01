ee161a4890cdb0506e5f1ac1da400c2da4ac1bb2
Taofiq-Bakare <taofiq01@gmail.com>
2021-05-02 05:47:15 +0800

    My first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Intro_to_pytorch',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a series of beginer level tutorial to masater pytorch.',
+    author='Taofiq-Bakare',
+    license='MIT',
+)
