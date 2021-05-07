0b877dfab6de785d72f313b3ed0040a6d488fcc9
Taofiq-Bakare <taofiq01@gmail.com>
2021-05-08 02:21:42 +0800

    Edited the auto gradient descent file

--- models/gradient_descent_auto.py
+++ models/gradient_descent_auto.py
@@ -1,1 +1,16 @@
 # Here I convert the manual gradient descent to auto with Pytorch.
+
+
+import torch
+
+# Recap steps
+
+'''
+
+1. A forward pass
+2. Loss/error calculation
+3. Back propagation/optimization/weight correction
+4. Training loop
+5. Make predictions.
+
+'''
